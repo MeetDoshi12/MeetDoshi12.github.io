d23ba9bb61a6856a9b23d8a0b050ea5d6d268397
MeetDoshi12 <doshimeet1211@gmail.com>
2024-07-12 16:18:26 +0530

    my website

--- script.js
+++ script.js
@@ -49,31 +49,34 @@ window.onscroll = function() {
 }
 
 function thanks() {
-	var name = document.getElementsByName('Name')[0].value;
-	var email = document.getElementsByName('Email')[0].value;
-	var comments = document.getElementsByName('Comments')[0].value;
-  
-	var data = {
-	  'Name': name,
-	  'Email': email,
-	  'Comments': comments
-	};
-  
-	fetch('https://script.google.com/macros/s/AKfycbyNGlI_D64V7wLBC-ZB3uyAwDy_awbl98WxtLSG598vMLA5KHpixQ7U4zoBUZ65E0qwow/exec', {
-	  method: 'POST',
-	  headers: {
-		'Content-Type': 'application/json',
-	  },
-	  body: JSON.stringify(data)
-	})
-	.then(response => {
-	  if (response.ok) {
-		alert("Thanks for your feedback!");
-	  } else {
-		throw new Error('Network response was not ok.');
-	  }
-	})
-	.catch(error => {
-	  console.error('There has been a problem with your fetch operation:', error);
-	});
-  }  
+    var name = document.getElementsByName('Name')[0].value;
+    var email = document.getElementsByName('Email')[0].value;
+    var comments = document.getElementsByName('Comments')[0].value;
+
+    var data = {
+        'Name': name,
+        'Email': email,
+        'Comments': comments
+    };
+
+    // Send POST request to Google Apps Script API
+    fetch('https://script.google.com/macros/s/AKfycbyNGlI_D64V7wLBC-ZB3uyAwDy_awbl98WxtLSG598vMLA5KHpixQ7U4zoBUZ65E0qwow/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Thanks for your feedback!");
+        } else {
+            throw new Error('Network response was not ok');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert("There was an error submitting your feedback. Please try again later.");
+    });
+}
+ 
